dfa93f1f38c86de4c56876b0b35b11644acdc9c3
n4oji <naoji.okamoto@gmail.com>
2023-03-11 22:35:22 -0300

    profile list

--- social/Meeps/views.py
+++ social/Meeps/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .models import Profile
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles": profiles})
+    else:
+        messages.success(request, ("Voce precisa estar logado para ver!"))
+        return redirect('home')
